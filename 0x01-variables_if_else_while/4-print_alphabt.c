b25dc2a7317d044981d6483da8a5f2ca89649df8
kwa090 <danny4yesu@gmail.com>
2022-10-16 17:42:30 -0700

    Creatings Numbers

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Write a program that prints the alphabet in lowercase
+ * Return: Always 0
+ */
+int main(void)
+{
+	char la;
+
+	for (la = 'a'; la <= 'z'; la++)
+	{
+		if (la != 'e'; && la != 'q')
+			putchar(la);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
