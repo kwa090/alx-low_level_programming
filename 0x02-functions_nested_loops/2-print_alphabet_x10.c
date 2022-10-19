cda7c6818fc3c16578a0f323e73fdc3da17036b6
kwa090 <danny4yesu@gmail.com>
2022-10-18 21:00:55 -0700

    Functions, nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include <main.h>
+
+/**
+ * main - a function that prints 10 times the alphabet, in lowercase
+ * Return: Always 0
+ */
+void print_alphabet_x10(void);
+{
+	int ten;
+	char la;
+
+	for (ten = '0'; ten <= '9'; ten++)
+	{
+		for (la = 'a'; la <= 'z'; la++)
+		{
+			_putchar(la);
+		}
+
+		_putchar('\n');
+
+		return (0);
+	}
+}
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include <main.h>
+
+/**
+ * main - function that checks for lowercase character @c: the character
+ * Return: 1 if the letter is loercase, 0 if not
+ */
+int _islower(int c)
+{
+	if (c >= 'a'; &&c <= 'z')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
