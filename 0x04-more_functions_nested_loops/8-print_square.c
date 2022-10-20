3fca066e2db1c78b59cddc4d913289ffe7edbaa5
kwa090 <danny4yesu@gmail.com>
2022-10-20 15:02:06 -0700

    function that prints a square, followed by a new line

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,8 +1,8 @@
 #include "main."
 
 /**
- * print_square - print a square
- * @size:size of the square
+ * print_square - prints a square
+ * @size: size of the square
  */
 void print_square(int size)
 {
