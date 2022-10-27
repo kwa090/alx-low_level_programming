6957cf3e788cc769610900d6c2b6e4849110e825
kwa090 <danny4yesu@gmail.com>
2022-10-27 16:40:59 -0700

    task06

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,30 +1,35 @@
 #include "main.h"
 
 /**
- * cap_string - capitalizes all words of a string
- * @s: input string.
- * Return: the pointer to dest.
+ * cap_string - capitalizes every word in a string
+ * @s: String to capitalize words of
+ * Return: Pointer to string
  */
 char *cap_string(char *s)
 {
-	int count = 0, i;
-	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+	int i = 0;
 
-	if (*(s + count) >= 97 && *(s + count) <= 122)
-		*(s + count) = *(s + count) - 32;
-	count++;
-	while (*(s + count) != '\0')
+	while (s[i] != 0)
 	{
-		for (i = 0; i < 13; i++)
-		{
-			if (*(s + count) == sep_words[i])
-			{
-				if (((s + (count + 1)) >= 97) && ((s + (count + 1)) <= 122))
-					*(s + (count + 1)) = *(s + (count + 1)) - 32;
-				break;
-			}
-		}
-		count++;
+		if (s[i] >= 'a' && s[i] <= 'z')
+	{
+		if (i == 0)
+			s[i] -= 32;
+		else if (s[i - 1] == ',' ||
+				s[i - 1] == ';' ||
+				s[i - 1] == '.' ||
+				s[i - 1] == '!' ||
+				s[i - 1] == '"' ||
+				s[i - 1] == ')' ||
+				s[i - 1] == '(' ||
+				s[i - 1] == '{' ||
+				s[i - 1] == '}' ||
+				s[i - 1] == '\n' ||
+				s[i - 1] == 32 ||
+				s[i - 1] == '\t')
+			s[i] -= 32;
+	}
+		i++;
 	}
 	return (s);
 }
