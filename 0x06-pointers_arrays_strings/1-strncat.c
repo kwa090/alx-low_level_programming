0b33010420f4499a810a5bf19669bcb020b6c443
kwa090 <danny4yesu@gmail.com>
2022-10-26 17:37:27 -0700

    task1-2

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,14 +13,14 @@ char *_strncat(char *dest, char *src, int n)
 	int j;
 
 	i = 0;
-	while (des[i] != '\0')
+	while (dest[i] != '\0')
 	{
 		i++;
 	}
 	j = 0;
 	while (j < n && src[j] != '\0')
 	{
-		(dset[i] = src[j];
+		(dest[i] = src[j];
 		i++;
 		j++;
 	}
