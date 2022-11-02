a62907e3c0b37528764ab5fddaa52e58d570e34f
kwa090 <danny4yesu@gmail.com>
2022-11-02 03:10:18 -0700

    task 0-1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - reverses a string
+ * @s: input
+ * Return: Always 0
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion
+		_putchar(*s);
+	}
+}
