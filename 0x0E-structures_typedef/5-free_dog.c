763cb2d1b3e2624fbb5efdf89a9ac71bb6870541
kwa090 <danny4yesu@gmail.com>
2022-11-15 13:44:42 -0800

    5-free_dog.c

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,4 +1,3 @@
-5-free_dog.c
 #include "dog.h"
 #include <stdlib.h>
 
@@ -14,32 +13,3 @@ void free_dog(dog_t *d)
 	free(d->name);
 	free(d);
 }
-
-dog.h
-#ifndef DOG_H
-#define DOG_H
-
-/**
- * struct dog - A new type descriding a dog.
- * @name: The name of the dog.
- * @age: The age of the dog.
- * @owner: The owner of the dog.
- */
-struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-};
-
-/**
- * dog_t - Typedef for struct dog
- */
-typedef struct dog dog_t;
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-
-#endif
